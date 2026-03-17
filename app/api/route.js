486e524ffbe6f527daecc643b0ae0c41d59bf05a
nutanchakma <newtonchakma1947@gmail.com>
2026-03-17 11:00:54 +0530

    feat: add user deletion sync

--- app/api/route.js
+++ app/api/route.js
@@ -1,13 +1,19 @@
 import { serve } from "inngest/next";
-import { inngest, syncUserCreation, syncUserUpdation } from "@/config/inngest";
+import { 
+  inngest, 
+  syncUserCreation, 
+  syncUserUpdation, 
+  syncUserDeletion   // ✅ ADD THIS
+} from "@/config/inngest";
 
 export const dynamic = "force-dynamic";
 
-// Create an API that serves zero functions
+// Create an API that serves functions
 export const { GET, POST, PUT } = serve({
   client: inngest,
   functions: [
     syncUserCreation,
-    syncUserUpdation
+    syncUserUpdation,
+    syncUserDeletion   // ✅ ADD THIS
   ],
 });
